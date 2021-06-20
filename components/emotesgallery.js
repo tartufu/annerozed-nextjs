925f1d801e782072eb5436a83e2e30ad24028942
tartufu <ainurlindale@gmail.com>
2021-06-20 19:50:55 +0800

    updated emotes gallery

--- components/emotesgallery.js
+++ components/emotesgallery.js
@@ -5,6 +5,8 @@ export default function EmotesGallery(props) {
       <div>
         <img src="images/emotes/teriwahhlive.gif" />
         <img src="images/emotes/lulupunch.gif" />
+        <img src="images/emotes/ayame_money.gif" />
+        <img src="images/emotes/ayamelaugh.gif" />
       </div>
       <div className={props.styles.emotesgallerypng}>
         <img src="images/emotes/rozashouri.png" />
@@ -17,12 +19,12 @@ export default function EmotesGallery(props) {
         <img src="images/emotes/luluhuhh2.png" />
         <img src="images/emotes/luluknuckle.png" />
         <img src="images/emotes/stronk.png" />
-        <img src="images/emotes/sopbye2.png" />
-        <img src="images/emotes/sopbye3.png" />
-        <img src="images/emotes/sopno.png" />
-        <img src="images/emotes/sopno2.png" />
+        <img src="images/emotes/HimeWink.png" />
+        <img src="images/emotes/XnLrupap.png" />
         <img src="images/emotes/lilipray.png" />
         <img src="images/emotes/kanami_final.png" />
+        <img src="images/emotes/durastare.png" />
+        <img src="images/emotes/duralmao.png" />
       </div>
     </div>
   );
